5683d2a0627dab7b9546f901d91815c5101ee9ca
Amandine ESPAGNET <amandine.espagnet33@gmail.com>
2024-04-29 19:07:04 +0200

    deuxieme version deuxieme essai

--- toto.c
+++ toto.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+
+int main(int argc, char **argv)
+{
+  int number_one = 1;
+  int number_two = 2;
+  return max(number_one, number_two);
+}
